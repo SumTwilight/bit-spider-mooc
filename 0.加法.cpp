4c6d973dbaa50c1292b4bb7769e64f69cc093c2b
SummerTwilight <ncutzl@qq.com>
2018-10-25 18:55:34 +0800

    在vs中修改后提交

--- 0.加法.cpp
+++ 0.加法.cpp
@@ -1,14 +1,16 @@
 ﻿#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 	long long a, b;
 	scanf("%lld%lld", &a, &b);
 	printf("%lld\n", a + b);
 	if(1+1==2)
-		printf("OK");
-	printf("修改分支，测试如何上传到网上服务器");
+		printf("OK\n");
+	printf("adf 张");
 //啊啦啦啦啦啦
 //第二次修改上传
 //在线上修改后下载
+	system("pause");
 	return 0;
 }
